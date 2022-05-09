6f8289e37979e75bea604d90123a0f036bf6834f
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-05-09 14:50:15 +0530

    auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import LoginButton from "./component/LoginButton";
+import LogoutButton from "./component/LogoutButton";
+import Profile from "./component/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 function App() {
+  const { isLoading } = useAuth0();
+  if (isLoading) return <div>Loading....</div>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </div>
   );
 }
---
+++ src/component/Profile.tsx
@@ -0,0 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+function Profile(): JSX.Element | any {
+  const { user, isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user?.picture} alt={user?.name} />
+        <h2>{user?.name}</h2>
+        <p>{user?.email}</p>
+      </div>
+    )
+  );
+}
+
+export default Profile;
